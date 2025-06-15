31ad69ce45ecceed0a3857639ab5d29b5a886147
Sauravrp67 <paudelsaurav59@gmail.com>
2025-06-15 14:36:54 +0545

    Completed Dataloader

--- pytorch_dataloader.py
+++ pytorch_dataloader.py
@@ -12,24 +12,31 @@ class Dataloader():
         self.valid_size = valid_size
         self.test = test
     
-    def data_loader(self):
+    def get_data_loader(self):
+        # We need to normalize each channel with a mean and std values so that exploding gradients problem don't occur.
+        # Also, the initial weights assumes (He,Xavier) which assume input features are zero-centered.
         normalize = transforms.Normalize(mean = [0.5071,0.4867,0.4408],std = [0.2675,0.2565,0.2761])
+        # Resizing the image size making it suitable for VGG network.
         transform_object = [transforms.Resize((224,224)),transforms.ToTensor(),normalize]
         transform = transforms.Compose(transform_object)
         #Testing Set
         if self.test:
+            print("downloading started...")
             dataset = datasets.CIFFAR10(
                 root = self.data_dir,
                 train = False,
                 download=True,
                 transform = transform
             )
-
+            print("Downloading Completed...")
             dataloader = torch.utils.data.DataLoader(
                 dataset,
                 batch_size = self.batch_size,
                 shuffle = self.shuffle,
-                pin_memory = True,
+                #Pinning Memory means storing the tensors in non-pageable memory of virtual memory,
+                #Meaning they remain fixed in the RAM once loaded and OS cannot swap these segments to Seconday storage during runtime
+                #This reduces the latency of transferring cpu tensor to GPU during training loop
+                pin_memory = True, 
                 num_workers = 2,
 
             )
