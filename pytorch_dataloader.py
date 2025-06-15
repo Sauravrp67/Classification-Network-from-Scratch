f7bc13fd2eeaf8403cf2aca26b215ec5059bdfcf
Sauravrp67 <paudelsaurav59@gmail.com>
2025-06-15 13:09:01 +0545

    Added Dataloader Module

---
+++ pytorch_dataloader.py
@@ -0,0 +1,61 @@
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+import torch
+import numpy as np
+class Dataloader():
+    def __init__(self,data_dir,batch_size,random_seed=42,shuffle=True,valid_size = 0.1,test=False):
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.random_seed = random_seed
+        self.shuffle = shuffle
+        self.valid_size = valid_size
+        self.test = test
+    
+    def data_loader(self):
+        normalize = transforms.Normalize(mean = [0.5071,0.4867,0.4408],std = [0.2675,0.2565,0.2761])
+        transform_object = [transforms.Resize((224,224)),transforms.ToTensor(),normalize]
+        transform = transforms.Compose(transform_object)
+        #Testing Set
+        if self.test:
+            dataset = datasets.CIFFAR10(
+                root = self.data_dir,
+                train = False,
+                download=True,
+                transform = transform
+            )
+
+            dataloader = torch.utils.data.DataLoader(
+                dataset,
+                batch_size = self.batch_size,
+                shuffle = self.shuffle,
+                pin_memory = True,
+                num_workers = 2,
+
+            )
+            return dataloader
+        
+        train_dataset = datasets.CIFAR10(
+            root=self.data_dir,
+            train = True,
+            download = True,
+            transform = transform
+        )
+        num_train = len(train_dataset)
+        indices = list(range(num_train))
+        split = int(np.floor(self.valid_size * num_train))
+
+        if self.shuffle:
+            np.random.seed(self.random_seed)
+            np.random.shuffle(indices)
+        train_idx, valid_idx = indices[split:],indices[:split]
+        train_sampler = SubsetRandomSampler(train_idx)
+        valid_sampler = SubsetRandomSampler(valid_idx)
+
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset,batch_size = self.batch_size,sampler = train_sampler,pin_memory = True,num_workers = 2
+        )
+        valid_laoder = torch.utils.data.DataLoader(
+            train_dataset,batch_size = self.batch_size,sampler = valid_sampler, pin_memory = True,num_workers = 2
+        )
+        return (train_loader,valid_laoder)
